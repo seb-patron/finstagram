24a3c1014f1dd6795b5907d33c8974613667863d
Sebastian Patron <sebastian.patrones@gmail.com>
2018-06-27 15:59:23 +0200

    verify that file attached is actually an image, and is less than 500kb in size

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+     has_one_attached :image #each post has an attached image
+     # make sure uploaded file is an image, and is not larger than 500kb
+     validates :image, file_size: { less_than_or_equal_to: 500.kilobytes }, file_content_type: { allow: ['image/jpeg', 'image/png'] }
 end
