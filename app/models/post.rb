cbf75306360f7d85a1fd8c3f9be60564bce58cca
Sebastian Patron <sebastian.patrones@gmail.com>
2018-07-02 13:56:04 +0200

    user_id requried for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
      has_one_attached :image #each post has an attached image
      belongs_to :user # each post belongs to a user
+     validates :user_id, presence: true
      # make sure uploaded file is an image, and is not larger than 500kb
-     validates :user_id, presence :true
      validates :image, file_size: { less_than_or_equal_to: 500.kilobytes }, file_content_type: { allow: ['image/jpeg', 'image/png'] }
 end
